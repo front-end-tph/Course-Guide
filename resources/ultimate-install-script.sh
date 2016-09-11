0b30c36d12f936a516b9e3a6d1389b153c169195
Travis Hubbard <travis-tiy@Traviss-MacBook-Pro.local>
2016-09-11 13:21:09 -0400

    updating Course Guide

---
+++ resources/ultimate-install-script.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+
+#HOW TO USE THIS SCRIPT:
+##   download this file, or copy and paste all of its text into a file on your machine.
+##   navigate in the terminal to the location of the file.
+##   execute it using the command "bash <filename>". for example, "bash ultimate-install-script.sh."
+
+set -e
+
+echo 'Boostrapping your computer...'
+
+# Install xcode command line tools
+if [[ ! -x `xcode-select -p 2>/dev/null` ]]; then
+  xcode-select --install
+  echo 'Press any key when the installation has completed.'
+  read -n 1
+fi
+
+# Install homebrew
+if [[ ! -x /usr/local/bin/brew ]]; then
+  echo 'Installing homebrew...'
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+###############################################################################
+# SSH
+###############################################################################
+
+ssh-keygen
+
+#############
+
+# setup casks/taps
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# terminal utils
+binaries=(
+  coreutils # Install GNU core utilities (those that come with OS X are outdated)
+  findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+  bash # Install Bash 4
+  graphicsmagick
+  webkit2png
+  ffmpeg
+  python
+  git
+  node
+  npm
+  hub
+  wget
+)
+
+# Apps
+apps=(
+  google-chrome
+  atom
+  flux
+  skitch
+  Caskroom/cask/slack
+)
+
+# "installing binaries..."
+brew install ${binaries[@]}
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+# "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+brew cleanup
+
+$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+# Ask for the administrator password upfront
+echo "enter admin password NAOW t('.'t)"
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# General UI/UX
+###############################################################################
+
+# GateKeeper off
+sudo spctl --master-disable
+sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# "Increasing the window resize speed for Cocoa applications"
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+# "Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+# "Displaying ASCII control characters using caret notation in standard text views"
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+
+# "Disabling system-wide resume"
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# "Disabling automatic termination of inactive apps"
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+# "Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# "Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window"
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# "Check for software updates daily, not just once per week"
+# defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# "Remove duplicates in the Ã¢â‚¬Å“Open WithÃ¢â‚¬Â menu"
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# "Disable smart quotes and smart dashes as they're annoying when typing code"
+defad
+
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+###############################################################################
+
+# "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# "Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# "Setting a blazingly fast keyboard repeat rate (ain't nobody got time fo special chars while coding!)"
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# "Disabling auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# "Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+# "Turn off keyboard illumination when computer is not used for 5 minutes"
+defaults write com.apple.BezelServices kDimTime -int 300
+
+###############################################################################
+# Screen
+###############################################################################
+
+# "Requiring password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# "Setting location to ~/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+# "Setting screenshot format to PNG"
+defaults write com.apple.screencapture type -string "png"
+
+# "Enabling subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+ z
+# "Enabling HiDPI display modes (requires restart)"
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+###############################################################################
+# Finder
+###############################################################################
+
+# "Showing icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+# "Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# "Showing status bar in Finder by default"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# "Allowing text selection in Quick Look/Preview in Finder by default"
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# "Displaying full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# "Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# "Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+# "Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# "Disabling disk image verification"
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# "Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+
+###############################################################################
+# Dock & Mission Control
+###############################################################################
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don't use
+# the Dock to launch apps.
+#defaults write com.apple.dock persistent-apps -array
+
+# "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 36
+
+# "Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+# "Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+
+###############################################################################
+# Safari & WebKit
+###############################################################################
+
+# "Hiding Safari's bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# "Hiding Safari's sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+# "Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+# "Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# "Making Safari's search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+# "Removing useless icons from Safari's bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+# "Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+# "Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+# "Adding a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+
+###############################################################################
+# Mail
+###############################################################################
+
+# "Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+
+###############################################################################
+# Terminal
+###############################################################################
+
+# "Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+
+###############################################################################
+# Time Machine
+###############################################################################
+
+# "Preventing Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# "Disabling local Time Machine backups"
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+
+###############################################################################
+# Messages                                                                    #
+###############################################################################
+
+# "Disable smart quotes as it's annoying for messages that contain code"
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+
+# "Disable continuous spell checking"
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+
+###############################################################################
+# Transmission.app                                                            #
+###############################################################################
+
+# "Use `~/Downloads/Incomplete` to store incomplete downloads"
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
+
+# "Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+# "Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+# "Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+# "Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+
+###############################################################################
+# Personal Additions
+###############################################################################
+
+# "Disable hibernation (speeds up entering sleep mode)"
+sudo pmset -a hibernatemode 0
+
+# "Remove the sleep image file to save disk space"
+sudo rm /Private/var/vm/sleepimage
+# "creating a zero-byte file instead"
+sudo touch /Private/var/vm/sleepimage
+# "and make sure it can't be rewritten"
+sudo chflags uchg /Private/var/vm/sleepimage
+
+# "Disable the sudden motion sensor as it's not useful for SSDs"
+sudo pmset -a sms 0
+
+# "Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+# "Disable annoying backswipe in Chrome"
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
---
+++ syllabus.md
@@ -0,0 +1,81 @@
+### TIY Houston Front End Engineering Syllabus
+
+**Instructor Contact Info**
+
+- Travis Hubbard
+- [travis.hubbard@theironyard.com](mailto:travis.hubbrd@theironyard.com)
+- github: [t3patterson](http://github.com/t3patterson)
+
+**Campus Director Contact Info**
+
+- Betsy Hare
+- [betsy@theironyard.com](mailto:betsy@theironyard.com)
+
+**Schedule**
+
+- Lecture is from `9:00 AM to 12:00 Monday-Thursday`, with breaks during that time. Depending on the topic, we may end earlier or later. :-)
+- Lab time is from `1 PM to 5 PM Monday-Thursday` and `9 AM to 5 PM` on Friday!
+- Reviews with the TA will take place Tuesdays and Thursdays from 4 p.m. - ? (2+ hrs)
+- #IronPints Friday at 3.30pm at our local bars.
+
+**Office hours and one-on-one instruction**
+
+A teacher or TA will be available Monday-Friday from 9 AM to 5 PM at minimum. During lab time, you can schedule one-on-one time with either of us. After hours, we may be available in the classroom or in Slack, which we use for group communication.
+
+**Holidays**
+
+Thanksgiving
+  - *November 24 - (Thursday)*
+  - *November 25 - (Friday)*
+
+**Materials**
+
+All students must bring their own MacBook laptop to class. We recommend the 13" MacBook Air or Pro. The only upgrade (if any) you should purchase is 8GB of RAM.
+
+Your MacBook must be running [OSX El Capitan](https://itunes.apple.com/us/app/os-x-el-capitan/id1018109117?mt=12), which is a free upgrade from the App Store. [Download and install it](https://itunes.apple.com/us/app/os-x-el-capitan/id1018109117?mt=12) if you haven't yet.
+
+Accounts to sign up for:
+
+If you don't have an account with any of these, sign up for them now:
+
+1. http://codepen.io
+2. http://github.com
+3. http://medium.com
+4. http://trello.com (sign-in with your Google account for easier access)
+
+There is no commercial software required for the class. We will install a number of tools to be used, such as a code editor ([Atom](https://atom.io/)) and a chat program([Slacl](https://slack.com/)), but they will all be free and/or open-source.
+
+A custom [install script](https://github.com/TIY-Houston-Front-End-Engineering/Course-Guide/blob/master/Resources/system-resources/ultimate-install-script.sh) will be provided and run the first day of class. This script will install the necessary tools onto your Mac and also modify some of the settings to optimize the machine for programming, battery life, and other improved features.
+
+**Prework**
+
+See [The Prework document](./prework.md).
+
+**Final Projects**
+
+See [The Final Project document](./final-project.md).
+
+**After class ends**
+
+Blog! ~~> http://blog.theironyard.com/2015/02/25/bb-workshop/
+
+Learn server-side JS (Node.js) and other JS concepts with these CLI (command line interface) workshops: http://nodeschool.io/#workshoppers
+
+> Install them with npm (i.e. `npm install -g learnyounode`)
+
+
+**Policies**
+
+Sounds boring, but these are important:
+
+- Absences and timeliness - You can miss up to 4 lectures; after that, your enrollment in career support is forfeit. I don't recommend missing any, as we will be moving very fast! This is to help with expectations: I show up, you show up, we all show up --- and kick ass! :-) Also, please show up on time. We count 2 lates as an absence. All I ask for is general respect of the crew and class so that we can learn without constant hiccups and interruptions!
+
+- Late or incomplete homework - Your homework is due by 9 AM on the day of its deadline. After that, it is late. If you miss homework assignments, come talk to me, because I know problems and emergencies can happen. However, you will be required to make up for late/incomplete assignments, so don't fall behind! I would rather see a solid attempt on time than a complete assignment late. Solid attempts are not considered late!
+
+- Honor - You should use all resources available to you, including open-source code, but copy and pasting open-source code will not only render your homework unacceptable but will also prevent you from learning the material (and often from writing code that works). Copying homework can result in forfeiting career support.
+
+- Conduct - I run a class based on human decency and respect - I want everyone to be welcome. Generally speaking, if something can be taken the wrong way, be careful how you say it! Have an issue with someone? **If you ever have any issues, just reach out to me or any of The Iron Yard employees. I promise we will represent everyone fairly and equally, as we genuinely care for the well-being of our students.**
+
+**Code School Manifesto**
+
+> http://masondesu.github.io/code-school-manifesto/
