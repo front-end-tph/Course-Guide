2b1a3288e3602f4510cf8d77f3416135b5421019
Travis Hubbard <t3patterson@gmail.com>
2016-09-27 17:57:47 -0400

    updating the task-10

--- assignments/10-js-iterables-basics/loops-iterables-basics.js
+++ assignments/10-js-iterables-basics/loops-iterables-basics.js
@@ -13,32 +13,11 @@ var simpList = [
     "Maggie"
 ]
 
-var modifiedNamesList = simpOrSamp(modifiedNamesList)
+var modifiedNamesList = simpOrSamp(simpList)
 log( modifiedNamesList[1] === "OJ Sampson" )
 log( modifiedNamesList[2] === "Marge Sampson" )
 log( modifiedNamesList[5] === "Maggie Simpson" )
 
-
-////------------------------------------------------------------------
-// TASK 2 : Build a function that returns an array of strings with last names added:
-//    If less than 6 characters, append Sampson: e.g. "Homer Sampson"
-//    If 6 characters or longer, append Simpson: 'Jessica Simpson'
-////------------------------------------------------------------------
-
-var simpList = [
-    "Homer",
-    "OJ",
-    "Marge",
-    "Jessica",
-    "Bart",
-    "Maggie",
-    "Abraham"
-]
-
-var simpSampList = simpOrSamp(modifiedNamesList)
-console.assert( simpSampList[1] === "OJ Sampson" )
-console.assert( simpSampList[2] === "Marge Sampson" )
-console.assert( simpSampList[5] === "Maggie Simpson" )
 
 ////------------------------------------------------------------------
 // TASK 2 : Write a function called shortiesOnly(). It should as input an array of strings, and
