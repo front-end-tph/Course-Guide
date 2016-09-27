d2fea445ce0cfcf4d6f697194d103f9727a82e4c
Travis Hubbard <t3patterson@gmail.com>
2016-09-27 16:39:16 -0400

    updating basic iterables

---
+++ assignments/11-js-iterables-basics/loops-iterables-basics.js
@@ -0,0 +1,64 @@
+////------------------------------------------------------------------
+// TASK 1 : Build a function that returns an array of strings with last names added:
+//    If less than 6 characters, append Sampson: e.g. "Homer Sampson"
+//    If 6 characters or longer, append Simpson: 'Jessica Simpson'
+////------------------------------------------------------------------
+
+var simpList = [
+    "Homer",
+    "OJ",
+    "Marge",
+    "Jessica",
+    "Bart",
+    "Maggie"
+]
+
+var modifiedNamesList = simpOrSamp(modifiedNamesList)
+log( modifiedNamesList[1] === "OJ Sampson" )
+log( modifiedNamesList[2] === "Marge Sampson" )
+log( modifiedNamesList[5] === "Maggie Simpson" )
+
+
+////------------------------------------------------------------------
+// TASK 2 : Build a function that returns an array of strings with last names added:
+//    If less than 6 characters, append Sampson: e.g. "Homer Sampson"
+//    If 6 characters or longer, append Simpson: 'Jessica Simpson'
+////------------------------------------------------------------------
+
+var simpList = [
+    "Homer",
+    "OJ",
+    "Marge",
+    "Jessica",
+    "Bart",
+    "Maggie",
+    "Abraham"
+]
+
+var simpSampList = simpOrSamp(modifiedNamesList)
+console.assert( simpSampList[1] === "OJ Sampson" )
+console.assert( simpSampList[2] === "Marge Sampson" )
+console.assert( simpSampList[5] === "Maggie Simpson" )
+
+////------------------------------------------------------------------
+// TASK 2 : Write a function called shortiesOnly(). It should as input an array of strings, and
+//    it should return a new array containing only those strings with four or fewer characters.
+////------------------------------------------------------------------
+
+var allNames = [
+    "Ed",
+    "Tom",
+    "Elfrid",
+    "Sam",
+    "Bartholomeu",
+    "Wayne",
+    "Theodore",
+    "Ingrid",
+    "Fred"
+    "Yvette"
+]
+
+var shortList = shortiesOnly(allNames)
+console.assert( shortList.length === 4 )
+console.assert( shortList.indexOf('Fred') != -1 )
+console.assert( shortList.indexOf('Wayne') === -1 )
