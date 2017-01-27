1806b78e3243b6d4587658e62a81a223926c9ae7
Travis Hubbard <t3patterson@gmail.com>
2017-01-27 15:06:51 -0500

    updating project files

--- assignments/js-objects-intermediate/ex-07-showStats.js
+++ assignments/js-objects-intermediate/ex-07-showStats.js
@@ -10,8 +10,8 @@
  * 
  *  Ex:   showStats(3, [1,2,3,4,5,6,5,4,3,2,1])
  *          => { 
-  *                count_inputEqualTo: 2, 
- *					    count_inputGreaterThan: 4, 
+ *                 count_inputEqualTo: 2, 
+ *	                count_inputGreaterThan: 4, 
  *                 count_inputLessThan: 5, 
  *                 setSize: 11 
  *              } 
